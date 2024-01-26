8f17fb04d4b536526d9686d050d97125f55ca804
Helena Saborit <helena.sb91@gmail.com>
2024-01-26 13:09:49 +0100

    changed Favicon + updated error page

--- src/pages/cosplay-pages/ChoosedCosplay.jsx
+++ src/pages/cosplay-pages/ChoosedCosplay.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import MissingMeme from "../../assets/Missing-meme.png";
 
 function ChoosedCosplay({status}) {
   return (
     <div className="choosed-cosplay">
        
-          <h2>UPS, LLEGAS TARDE!!</h2>
-          <h3>Parece que otra persona ha sido más rápida que tu...</h3>
-          <button className="btn btn-yellow"><Link to={"/cosplay/cosplay-list"}>Lista de Cosplays disponibles</Link></button>
-        
+          <h2>UPS! LLEGAS TARDE!!</h2>
+          <h5>Parece que otra persona ha sido más rápida que tú...</h5>
+          <button className="btn btn-yellow-cosplay"><Link to={"/cosplay/cosplay-list"}>Lista de Cosplays disponibles</Link></button>
+          <br />
+          <img src={MissingMeme} alt="Missing meme" />
+
+          
     </div>
 )}
 
